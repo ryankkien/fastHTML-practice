bc8164e535860b68c9603ff57be8decfa7d16206
ryankkien <93295033+ryankkien@users.noreply.github.com>
2024-08-05 16:23:37 -0700

    created sublink

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from fasthtml.common import *
 app,rt = fast_app(live=True)
 
 @rt('/') #router
+#http req
 def get(): return Titled('Greeting',
-                         Div(P('Alive!!'), hx_get="/change")
-                         )#http req
+                         Div(P('Alive!!')),
+                         P(A('Link', href = '/change'))
+                         )
 
+
+@rt('/change') #grabbed when link is clicked
+def get():
+        return Titled('Change',
+                      Div(P('Changed!!')),
+                    P(A('Home', href= '/'))
+        )
 serve()
