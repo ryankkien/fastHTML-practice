da4e47f86a000ed9684d59cc37e0153ae6ded806
ryankkien <93295033+ryankkien@users.noreply.github.com>
2024-08-05 16:27:06 -0700

    function

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ app,rt = fast_app(live=True)
 @rt('/') #router
 #http req
 def get():
-    nums = Ul(*[Li(o) for o in range(10)])
+    nums = createList(10)
     return Titled('Greeting',
                          Div(P('Alive!!')),
                          P(A('Link', href = '/change'))
                          )
 
+def createList(n):
+    return Ul(*[Li(o) for o in range(n)])
 
 @rt('/change') #grabbed when link is clicked
 def get():
